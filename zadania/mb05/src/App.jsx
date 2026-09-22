1f1b85f69f35a70e6133bc527cf692dc97384d48
anna <email@example.com>
2026-09-22 14:27:49 +0200

    komponent addphotomodal

--- zadania/mb05/src/App.jsx
+++ zadania/mb05/src/App.jsx
@@ -2,6 +2,8 @@ import Navbar from "./components/Navbar";
 import CategoryBar from "./components/CategoryBar";
 import Gallery from "./components/Gallery";
 import Footer from "./components/Footer";
+import AddPhotoModal from "./components/AddPhotoModal";
+
 function App() {
   return (
     <>
@@ -46,6 +48,7 @@ function App() {
         <Gallery />
       </main>
       <Footer />
+      <AddPhotoModal />
     </>
   );
 }
