0defd6055c41f76411eb86a4143ae6bb6913e238
anna <email@example.com>
2026-09-22 14:25:27 +0200

    komonent galerry i siatka kart z json

--- zadania/mb05/src/App.jsx
+++ zadania/mb05/src/App.jsx
@@ -1,5 +1,6 @@
 import Navbar from "./components/Navbar";
 import CategoryBar from "./components/CategoryBar";
+import Gallery from "./components/Gallery";
 
 function App() {
   return (
@@ -42,6 +43,7 @@ function App() {
 
       <main className="container">
         <CategoryBar />
+        <Gallery />
       </main>
     </>
   );
